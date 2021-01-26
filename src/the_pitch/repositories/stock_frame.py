df054cd0765971f8eeddea92d4da623e37301413
dpasse <dpasse@mpirik.com>
2021-01-26 09:25:01 -0600

    bug with multiple symbol indicator refreshing

--- src/the_pitch/repositories/stock_frame.py
+++ src/the_pitch/repositories/stock_frame.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+import numpy as np
 import pandas as pd
 from pandas.core.groupby import DataFrameGroupBy
 from ..indicators import AbstractIndicator
@@ -6,12 +7,19 @@ from ..domain import StockPrice, Portfolio
 
 
 class StockFrame():
-    def __init__(self, prices: List[StockPrice], indicators: List[AbstractIndicator] = [], cache_path: Optional[str] = None, **kwargs) -> None:
-        self.df = pd.DataFrame(data=[ price.to_obj() for price in prices ]).set_index(keys=StockPrice.index_columns())
-        self.cache_path = cache_path
+    def __init__(self, **kwargs) -> None:
+        if 'prices' in kwargs.keys():
+            self.df = pd.DataFrame(data=[ price.to_obj() for price in kwargs['prices'] ]).set_index(keys=StockPrice.index_columns())
 
-        self.indicators = indicators
-        self._refresh_indicators(**kwargs)
+        if 'df' in kwargs.keys():
+            self.df: pd.DataFrame = kwargs['df'].set_index(keys=StockPrice.index_columns())
+
+        self.cache_path = None
+        if 'cache_path' in kwargs:
+            self.cache_path: str = kwargs['cache_path']
+
+        self.indicators: List[AbstractIndicator] = kwargs['indicators']
+        self._refresh_indicators()
 
     @property
     def symbol_groups(self) -> DataFrameGroupBy:
@@ -27,8 +35,20 @@ class StockFrame():
     def _refresh_indicators(self, **kwargs) -> None:
         self.df.sort_index(inplace=True)
 
+        symbols = np.unique(
+            list(map(lambda index: index[0], self.df.index))
+        )
+
         for indicator in self.indicators:
-            self.df = indicator.compute(self.df, **kwargs)
+
+            calc = []
+            for symbol in symbols:
+                output = indicator.compute(self.df.loc[symbol], **kwargs).reset_index()
+                output['symbol'] = symbol
+
+                calc.append(output)
+
+            self.df = pd.concat(calc).set_index(keys=StockPrice.index_columns()).sort_index()
 
         self._cache()
 
